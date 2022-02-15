74a8b909e3091872f96e32666876a503865d4f7d
Ryan Howard <ryanghoward@protonmail.com>
2022-02-15 14:10:28 -0800

    Commit 3

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
     tbl.string("vin", 17).notNullable().unique();
     tbl.string("make", 100).notNullable();
     tbl.string("model", 100).notNullable();
-    tbl.numeric("milage").unsigned().notNullable();
+    tbl.integer("mileage").unsigned().notNullable();
     tbl.string("title", 100);
     tbl.string("transmission", 100);
   });
