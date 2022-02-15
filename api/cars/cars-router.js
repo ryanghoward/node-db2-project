1a2ba00a8813db080d99b9028b920206f2f7019d
Ryan Howard <ryanghoward@protonmail.com>
2022-02-15 15:26:50 -0800

    Commit 9

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Car = require("./cars-model");
-const { checkCarId } = require("./cars-middleware");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
 
 const router = express.Router();
 router.get("/", async (req, res, next) => {
@@ -16,8 +21,14 @@ router.get("/:id", checkCarId, async (req, res, next) => {
   res.json(req.car);
 });
 
-router.post("/", async (req, res, next) => {
-  res.json("posting new car");
-});
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    res.json("posting new car");
+  }
+);
 
 module.exports = router;
