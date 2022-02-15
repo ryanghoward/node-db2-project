39fbf90aa5f9a1b7bbce60e3e091bdaa3f39e6fd
Ryan Howard <ryanghoward@protonmail.com>
2022-02-15 14:34:58 -0800

    Commit 6

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const res = require("express/lib/response");
-
+const carsRouter = require("./cars/cars-router");
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/cars", carsRouter);
 
 server.use("*", (req, res, next) => {
   next({ status: 404, message: "not found" });
