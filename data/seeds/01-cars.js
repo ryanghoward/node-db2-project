fdd5b4ce8e5d979ae802c3502d4c404ff5019780
Ryan Howard <ryanghoward@protonmail.com>
2022-02-15 13:42:23 -0800

    Commit 1

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1111111111111",
+    make: "Honda",
+    model: "Civic",
+    mileage: 10000,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "2222222222222",
+    make: "Toyota",
+    model: "Corolla",
+    mileage: 100000,
+    title: "clean",
+  },
+  {
+    vin: "3333333333333",
+    make: "Ford",
+    model: "F150",
+    mileage: 200000,
+    title: "clean",
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
