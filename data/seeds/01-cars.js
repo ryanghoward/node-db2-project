446cbedbeac224e10a4a6645b8ca97c939efa2e8
Ryan Howard <ryanghoward@protonmail.com>
2022-02-15 15:43:56 -0800

    Commit 10

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
   {
-    vin: "11111111111111111",
+    vin: "5XYKT3A69DG353356",
     make: "Honda",
     model: "Civic",
     mileage: 10000,
@@ -9,14 +9,14 @@ const cars = [
     transmission: "manual",
   },
   {
-    vin: "22222222222222222",
+    vin: "JN1CA31D3YT717809",
     make: "Toyota",
     model: "Corolla",
     mileage: 100000,
     title: "clean",
   },
   {
-    vin: "33333333333333333",
+    vin: "2CNDL23F856093901",
     make: "Ford",
     model: "F150",
     mileage: 200000,
