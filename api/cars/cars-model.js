cdf156c7809a996a2713380df2c0ff89f0b5f188
Ryan Howard <ryanghoward@protonmail.com>
2022-02-15 14:49:07 -0800

    Commit 7

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,5 +1,15 @@
-const getAll = () => {};
+const db = require("../../data/db-config");
+
+const getAll = () => {
+  return db("cars");
+};
 
 const getById = () => {};
 
 const create = () => {};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const Car = require("./cars-model");
 
 const router = express.Router();
 router.get("/", async (req, res, next) => {
-  res.json("getting all cars");
+  try {
+    const cars = await Car.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/", async (req, res, next) => {
