f9ca5f7cf4f655fd25838a399d12ddb180b42e15
Ryan Howard <ryanghoward@protonmail.com>
2022-02-15 16:14:20 -0800

    Commit 11. All tests passing

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -8,10 +8,19 @@ const getById = (id) => {
   return db("cars").where("id", id).first();
 };
 
-const create = () => {};
+const getByVin = (vin) => {
+  return db("cars").where("vin", vin).first();
+};
+
+const create = (car) => {
+  return db("cars")
+    .insert(car)
+    .then(([id]) => getById(id));
+};
 
 module.exports = {
   getAll,
   getById,
+  getByVin,
   create,
 };
